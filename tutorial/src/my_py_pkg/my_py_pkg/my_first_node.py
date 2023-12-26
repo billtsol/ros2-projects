8e611736b3ba8931cc3aae9e7209960d8ed1bf6b
billtsol <billtsol@protonmail.com>
2023-12-27 01:24:28 +0200

    First simple python package

---
+++ tutorial/src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+
+  def __init__(self):
+    super().__init__('py_test')
+
+    self.counter = 0
+    self.get_logger().info('Hello ROS2')
+
+    self.create_timer(0.5, self.timer_callback) # 2Hz
+
+
+  def timer_callback(self):
+    self.get_logger().info('Hello ' + str(self.counter))
+    self.counter+=1
+
+
+def main(args=None):
+
+  rclpy.init(args=args)
+
+  node = MyNode()
+
+  rclpy.spin(node) # spin() will block until the node is shutdown
+
+  rclpy.shutdown()
+
+
+if __name__ == '__main__':
+  main()
---
+++ tutorial/src/my_py_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='billtsol',
+    maintainer_email='billtsol@protonmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # Give a name for the package and the name of the executable
+            "py_node = my_py_pkg.my_first_node:main"
+        ],
+    },
+)
