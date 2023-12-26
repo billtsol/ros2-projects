567044c7208aec2f1be94692d8d1dbec69b5a359
billtsol <billtsol@protonmail.com>
2023-12-27 01:24:42 +0200

    First simple c++ package

---
+++ tutorial/src/my_cpp_pkg/src/my_first_node.cpp
@@ -0,0 +1,15 @@
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char **argv)
+{
+
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("cpp_test");
+
+  RCLCPP_INFO(node->get_logger(), "Hello CPP World!");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}
