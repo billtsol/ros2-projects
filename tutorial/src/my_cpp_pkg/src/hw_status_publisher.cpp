656b3a81c6631ca0f4898094bf88a7e7be342e43
billtsol <billtsol@protonmail.com>
2023-12-28 18:29:43 +0200

    create c++ publisher of my custom interface

---
+++ tutorial/src/my_cpp_pkg/src/hw_status_publisher.cpp
@@ -0,0 +1,41 @@
+#include "rclcpp/rclcpp.hpp"
+#include "my_robot_interfaces/msg/hardware_status.hpp"
+
+class HardwareStatusPublisher : public rclcpp::Node
+{
+public:
+  HardwareStatusPublisher() : Node("hardware_status_publisher")
+  {
+
+    pub_ = this->create_publisher<my_robot_interfaces::msg::HardwareStatus>("hardware_status", 10);
+
+    timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&HardwareStatusPublisher::publishHardwareStatus, this));
+
+    RCLCPP_INFO(this->get_logger(), "Hardware Status Publisher Started");
+  }
+
+private:
+  void publishHardwareStatus()
+  {
+    auto msg = my_robot_interfaces::msg::HardwareStatus();
+    msg.temperature = 50;
+    msg.are_motors_ready = true;
+    msg.debug_message = "Everything is OK";
+    pub_->publish(msg);
+  }
+
+  rclcpp::Publisher<my_robot_interfaces::msg::HardwareStatus>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char **argv)
+{
+
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<HardwareStatusPublisher>();
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}
