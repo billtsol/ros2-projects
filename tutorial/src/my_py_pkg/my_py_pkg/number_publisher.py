7df5716da635ca242b7e86bdfab5119505813211
billtsol <billtsol@protonmail.com>
2023-12-28 21:18:50 +0200

    create launch file

--- tutorial/src/my_py_pkg/my_py_pkg/number_publisher.py
+++ tutorial/src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -9,10 +9,17 @@ class NumberPublisharNode(Node):
   def __init__(self):
     super().__init__('number_publisher')
 
-    self.number_ = 2
+    self.declare_parameter('number_to_publish', 2)
+    self.declare_parameter('publish_frequency', 1)
+
+    self.number_ = self.get_parameter('number_to_publish').value
+
+    self.publish_frequency_ = self.get_parameter('publish_frequency').value
+
+
     self.number_publisher_ = self.create_publisher(Int64, 'number', 10)
 
-    self.number_timer_ = self.create_timer(1.0,self.publish_number)
+    self.number_timer_ = self.create_timer(1.0 / self.publish_frequency_,self.publish_number)
 
 
     self.get_logger().info('Number Publisher has been started')
